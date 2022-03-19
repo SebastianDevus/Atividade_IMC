9d700aec7f4ffbbe96c6117d1ac4025856cdeb25
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 16:52:53 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double massa, altura, imc;
+
+Console.Write("Insira sua massa, em quilogramas: ");
+massa = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Insira sua altura, em metros: ");
+altura = Convert.ToDouble(Console.ReadLine()!);
+
+imc = massa / Math.Pow(altura, 2);
+
+Console.WriteLine($"\nSeu IMC é {imc} kg/m²");
